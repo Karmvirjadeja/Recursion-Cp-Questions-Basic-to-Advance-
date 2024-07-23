dcb2517b1236fbe359edcb9e42bd13a145926c17
Knight <112848592+Karmvirjadeja@users.noreply.github.com>
2024-07-23 20:56:28 +0530

    Create BacktrackingGenerateParenthesis.cpp

---
+++ BacktrackingGenerateParenthesis.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void generateParenthesis(string Output,int n,int open,int close,int idx){
+   if(idx==2*n){
+    cout<<Output<<endl;
+    return;
+   }
+
+   //Adding the opening brackets
+ if(open<n){
+    generateParenthesis(Output+'(',n,open+1,close,idx+1);
+ }
+
+   //Adding the closing brackets
+   if(close<open){
+    generateParenthesis(Output+')',n,open,close+1,idx+1);
+   }
+
+}
+int main(){
+    int n;
+    cin>>n;
+
+    generateParenthesis("",n,0,0,0);
+
+}
